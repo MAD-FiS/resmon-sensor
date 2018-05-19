a84459bf98a4187eac16a89af156915f90d81e80
lukasz22 <bartocha.lukasz@gmail.com>
2018-05-19 22:58:25 +0200

    modify help
    Change-Id: I27ef229e5db297fae333e45af6eabefa75892208

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import sensorClient
 
 if __name__ == '__main__':
     """Main function which provides argument parser and run Sensor Client"""
-    parser = argparse.ArgumentParser(description='Sensor client', epilog='Example usage: python3 main.py -a http://localhost:4000 -i 1 -b 10')
+    parser = argparse.ArgumentParser(description='Sensor client', epilog='Example usage: python3 main.py -a http://localhost:4001 -i 1 -b 10')
     parser.add_argument('-a', '--address', required=True, help='Full server address with protocol and port')
     parser.add_argument('-i', '--interval', required=True, type=int, help='Measurement interval [sec]')
     parser.add_argument('-b', '--buffer', type=int, help='Messages buffer size')
