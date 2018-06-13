e4e4e5c02494bfe6eedbb53ca15cd120e21feda0
Grzegorz-00 <grzegorz00new@gmail.com>
2018-06-13 19:54:40 +0200

    New scheme and metrics
    Change-Id: I66f50c1ecae1038d66e7b7840ea15bcd40feb469

--- src/test/testSensor.py
+++ src/test/testSensor.py
@@ -1,33 +1,48 @@
 import unittest
-from unittest.mock import MagicMock
-
 import sensor
-
+from unittest.mock import MagicMock
 
 class TestSensor(unittest.TestCase):
 
     def test_sensorBuffer(self):
         sensorObj = sensor.Sensor()
         sensorObj.getCpuUsage = MagicMock(return_value=20)
-        sensorObj.getRamUsage = MagicMock(return_value=40)
+        sensorObj.getCpuFrequency = MagicMock(return_value=2000)
+        sensorObj.getRamUsage = MagicMock(return_value=50)
+        sensorObj.getVirtualMemTotal = MagicMock(return_value=4000)
+        sensorObj.getVirtualMemAvailable = MagicMock(return_value=2000)
+        sensorObj.getLoggedUsersCount = MagicMock(return_value=10)
+        sensorObj.getProcessesCount = MagicMock(return_value=235)
         sensorObj.prepareModules()
         sensorBufferObj = sensor.SensorBuffer(sensorObj)
         sensorBufferObj.performMeasurement()
         data = sensorBufferObj.getData()
 
-        print(sensorObj.getRamUsage())
-
         self.assertEqual(len(data), 1)
-        self.assertEqual(data[0]['CPU_USAGE'], 20)
-        self.assertEqual(data[0]['RAM_USAGE'], 40)
+        self.assertEqual(data[0]['cpu_usage'], 20)
+        self.assertEqual(data[0]['cpu_frequency'], 2000)
+        self.assertEqual(data[0]['ram_usage'], 50)
+        self.assertEqual(data[0]['virtual_mem_total'], 4000)
+        self.assertEqual(data[0]['virtual_mem_available'], 2000)
+        self.assertEqual(data[0]['legged_users_count'], 10)
+        self.assertEqual(data[0]['processes_count'], 235)
 
     def test_sensor(self):
         sensorObj = sensor.Sensor()
         sensorObj.getCpuUsage = MagicMock(return_value=20)
-        sensorObj.getRamUsage = MagicMock(return_value=80)
+        sensorObj.getCpuFrequency = MagicMock(return_value=2000)
+        sensorObj.getRamUsage = MagicMock(return_value=50)
+        sensorObj.getVirtualMemTotal = MagicMock(return_value=4000)
+        sensorObj.getVirtualMemAvailable = MagicMock(return_value=2000)
+        sensorObj.getLoggedUsersCount = MagicMock(return_value=10)
+        sensorObj.getProcessesCount = MagicMock(return_value=235)
         sensorObj.prepareModules()
 
         data = sensorObj.getAllData()
-
-        self.assertEqual(data['CPU_USAGE'], 20)
-        self.assertEqual(data['RAM_USAGE'], 80)
+        self.assertEqual(data['cpu_usage'], 20)
+        self.assertEqual(data['cpu_frequency'], 2000)
+        self.assertEqual(data['ram_usage'], 50)
+        self.assertEqual(data['virtual_mem_total'], 4000)
+        self.assertEqual(data['virtual_mem_available'], 2000)
+        self.assertEqual(data['legged_users_count'], 10)
+        self.assertEqual(data['processes_count'], 235)
