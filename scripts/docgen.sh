fa62be3221374226031bc8bca030457067952f73
lukasz22 <bartocha.lukasz@gmail.com>
2018-06-18 03:12:13 +0200

    improve docgen
    Change-Id: Iff3cc27b2f2ecf66943e039c741adaf2a0e12fa9

--- scripts/docgen.sh
+++ scripts/docgen.sh
@@ -11,5 +11,14 @@ mkdir ./docs/
 MODULES=`find -name '*.py' | sed -e 's/\//./g' | sed -e 's/\.\.//g' \
     | sed -e 's/\.py//g' | sed 's|.__init__||'`
 echo $MODULES
-pydoc3 -w $MODULES
-for f in ./*.html ; do mv "$f" ./docs/ ; done
+
+OUT=`for f in $MODULES ; do \
+    pydoc3 -w $f; \
+    mv "$f.html" ./docs/ ; done`
+
+echo "$OUT"
+if [[ "$OUT" =~ "problem in" ]]; then
+    exit 1
+else
+    exit 0
+fi
